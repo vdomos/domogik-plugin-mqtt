5c9f1daed7f21d3f542e02b5c69a0d7001b818b3
vdomos <domos.vesta@gmail.com>
2017-02-04 15:25:54 +0100

    Add plugin parameter for old MQTT protocol, Add callback for new update devices without restarting plugin, Improve reconnexion after lost

--- lib/mqtt.py
+++ lib/mqtt.py
@@ -40,6 +40,7 @@ import threading
 import time
 import datetime
 import platform
+import socket
 import paho.mqtt.client as mqtt     # https://pypi.python.org/pypi/paho-mqtt
 
 # -------------------------------------------------------------------------------------------------
@@ -73,23 +74,27 @@ class MQTT:
     """ MQTT
     """
 
-    def __init__(self, log, cb_send, stop, mqtthost, mqttport, mqtttopic, devicelist):
+    def __init__(self, log, cb_send, stop, mqtthost, mqttport, mqttprotocol):
         """ INIT
         """
         self.log = log
         self.send = cb_send
-        self.stop = stop
-        self.devicelist = devicelist
+        self.stopplugin = stop
 
         self.mqtthost = mqtthost
         self.mqttport = mqttport
-        self.mqtttopic = mqtttopic        
+        self.mqttsubtopics = []        
         
-        self.MQTTClient = mqtt.Client('mqtt2dmg_' + platform.node())
+        protocollist = {"MQTTv31" : 3, "MQTTv311" : 4}
+        self.MQTTClient = mqtt.Client('mqtt2dmg_' + platform.node(), protocol=protocollist[mqttprotocol])
         # self.MQTTClient = mqtt.Client('mqtt2dmg_' + platform.node(), protocol=3)  # For mosquitto that don't support MQTT version 3.1.1 protocol
+        # protocol:  the version of the MQTT protocol to use for this client. Can be either MQTTv31 (3) or MQTTv311 (4)
+        ### self.MQTTClient.username_pw_set(mqtt_user, mqtt_pwd)   # For future use, Must be called before connect*()
         self.MQTTClient.on_connect = self.on_connect
         self.MQTTClient.on_message = self.on_message
         self.MQTTClient.on_disconnect = self.on_disconnect
+        
+        ### loop_start()/loop_stop() methods ?
 
 
     # -------------------------------------------------------------------------------------------------
@@ -99,40 +104,32 @@ class MQTT:
         if rc == 0:
             self.log.info(u"==> Connection to MQTT broquer successful, (Result code 0)")
             try:  
-                if len(self.mqtttopic) > 0:
-                    self.log.debug(u"==> Subscribe to Topic list:  %s" % format(self.mqtttopic))
-                    self.MQTTClient.subscribe(self.mqtttopic)   # Subscribing in on_connect() means that if we lose the connection and econnect then subscriptions will be renewed.
+                if self.mqttsubtopics:
+                    self.log.debug(u"==> Subscribe to Topic list:  %s" % format(self.mqttsubtopics))
+                    self.MQTTClient.subscribe(self.mqttsubtopics)   # Subscribing in on_connect() means that if we lose the connection and econnect then subscriptions will be renewed.
                 else:
                     self.log.info(u"==> No MQTT sensor topic to subscribe")
             except ValueError:      # Raises a ValueError if qos is not 0, 1 or 2, or if topic is None or has zero string length, or if topic is not a string, tuple or list.
-                errorstr = u"### Subscribing, invalid Qos or Topic in list:  %s" % format(self.mqtttopic)
-                self.log.error(errorstr)
-                self.MQTTClient.disconnect()
+                raise MQTTException(u"### Subscribing, invalid Qos or Topic in list:  %s" % format(self.mqttsubtopics))
         else:
-            self.log.error(u"### Connection to MQTT broquer failed, (Result code " + str(rc) + ": " + CONNECT_RESULT_CODES[rc] + ")")
-            self.MQTTClient.disconnect()                  # Stop the loop from trying to connect again if unsuccessful.
+            raise MQTTException(u"### Connection to MQTT broquer failed, (Result code " + str(rc) + ": " + CONNECT_RESULT_CODES[rc] + ")") 
 
 
     # -------------------------------------------------------------------------------------------------
     def on_disconnect(self, client, userdata, rc):
         """ The callback when disconnecting from MQTT server
         """
-        '''
-        if not self.stop.isSet():
+        if not self.stopplugin.isSet():
             self.log.error(u"### Connection to MQTT broquer has been lost.")
-            self.log.error(u"### Attempting to reconnect in 20s.")        # This will automatically reconnect if connection is lost.
-            time.sleep(20)
-            self.MQTTClient.connect(MQTTHOST, 1883, 60)
-        '''
-        self.log.error(u"### Disconnecting from MQTT broquer.")
-        self.stop.set()
+            self.log.error(u"### Waiting for the connection to be back")               # loop_start() will automatically reconnect if connection be back.
+        else:
+            self.log.info(u"==> MQTT client disconnected by plugin stop")
 
 
     # -------------------------------------------------------------------------------------------------
     def on_message(self, client, userdata, msg):
         """ The callback for when a PUBLISH message is received from the broquer.
         """
-        # Erreur "UnicodeDecodeError: 'ascii' codec can't decode byte" avec prevision pluie !
         self.log.info(u"==> Received subscribed MQTT message:  %s = %s" % (msg.topic, msg.payload))
         for deviceid in self.devicelist:        #  {'70' : {'name': 'Temp Atelier', 'topic': 'domogik/maison/ateliertemp', 'type' : 'mqtt.sensor_temperature', 'qos' : 0}}
             if msg.topic ==  self.devicelist[deviceid]["topic"]:
@@ -144,7 +141,8 @@ class MQTT:
                     if msg.payload not in ['0', '1']:
                         self.log.error(u"### MQTT message '%s' for device '%s' not return a binary: '%s'" % (msg.topic, self.devicelist[deviceid]["name"], msg.payload))
                         return
-                self.send(deviceid, msg.payload)
+                self.send(deviceid, msg.payload.decode('utf-8', 'ignore'))      # Erreur "UnicodeDecodeError: 'ascii' codec can't decode byte" avec prevision pluie !
+
 
     # -------------------------------------------------------------------------------------------------
     def connect(self):
@@ -154,21 +152,22 @@ class MQTT:
         try:
             self.MQTTClient.connect(self.mqtthost, int(self.mqttport), 60)
             self.log.info(u"==> Connected on MQTT broquer")
+            self.MQTTClient.loop_start()                        # This will automatically reconnect if connection is lost.
         except:
             error = u"### Error while connecting to MQTT broquer : %s " % str(traceback.format_exc())
             raise MQTTException(error)
-
 
     # -------------------------------------------------------------------------------------------------
     def mqttloop(self):
         """ Start listening to mqtt topic messages
         """     
-        while not self.stop.isSet():
-            self.MQTTClient.loop()
-        self.log.info(u"==> Stop listening MQTT message.")
+        self.log.info(u"==> Start MQTT loop")
+        while not self.stopplugin.isSet():
+            self.stopplugin.wait(3)
         self.MQTTClient.disconnect()
+        self.MQTTClient.loop_stop()
 
-
+    
     # -------------------------------------------------------------------------------------------------
     def pubcmd(self, topic, qos, value, deviceid):
         """ Publish the command value of the topic
@@ -187,6 +186,23 @@ class MQTT:
             errorstr = u"### Publishing value '%s' with '%s' topic for device name '%s' has failed." % (value, topic, self.devicelist[deviceid]["name"])
             self.log.error(errorstr)
             return False, errorstr
+    
+    
+    # -------------------------------------------------------------------------------------------------
+    def reloadMQTTDevices(self, devices, newsubtopics):
+        '''
+        '''
+        self.devicelist = devices
+        self.log.debug(u"==> Topics list:  old='%s', new='%s'" % (format(self.mqttsubtopics), format(newsubtopics)))
+        if not self.mqttsubtopics:                   # At start up the subscribe will be done in on_connect callback 
+            self.mqttsubtopics = newsubtopics
+            return
+        for topic in self.mqttsubtopics:
+            self.MQTTClient.unsubscribe(topic[0])
+        self.mqttsubtopics = newsubtopics
+        self.log.debug(u"==> Subscribe to new Topic list:  %s" % format(self.mqttsubtopics))
+        self.MQTTClient.subscribe(self.mqttsubtopics)
+    
     
     # -------------------------------------------------------------------------------------------------
     def is_number(self, s):
